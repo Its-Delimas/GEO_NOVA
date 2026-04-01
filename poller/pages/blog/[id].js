ae7970b762aa4b019f7c1d12e151ecec5c095921
Its-Delimas <spencerdelimas@gmail.com>
2026-04-02 00:00:50 +0300

    feat: dynamic content ~ continuation

--- poller/pages/blog/[id].js
+++ poller/pages/blog/[id].js
@@ -1,10 +1,15 @@
 import { useRouter } from 'next/router'
+import posts from '../../posts.json'
+
 const Random = () => {
     const router = useRouter();
+
+    const post = posts[router.query.id]
+    if (!post) return <p></p>
     return (
         <>
-            <h1>Blog post</h1>
-            <p>Post id: {router.query.id}</p>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
         </>
     )
 }
