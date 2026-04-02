8462b1a1b2c0afee90dae52e56c283de80d78862
Its-Delimas <spencerdelimas@gmail.com>
2026-04-02 07:56:54 +0300

    feat: Populating the head tag with custom tags

---
+++ poller/components/House.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+const House = () => (
+    <div>
+        <head>
+            <title>The page title</title>
+        </head>
+        {/* the rest jsx */}
+    </div>
+)
+export default House;
--- poller/pages/index.js
+++ poller/pages/index.js
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 
-const Index = () =>(
+
+
+const Index = () =>{
+return (
+
     <div>
         <h1>Home page</h1>
+        <style jsx>{`h1{font-size:3rem;}`}</style>
         <Link href='/blog'>
             <button>Blog</button>
         </Link>
     </div>
 )
+}
 
 export default Index;
