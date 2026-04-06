a7099704476a1da45099c7a344b1770363fe0d10
Its-Delimas <spencerdelimas@gmail.com>
2026-04-06 14:05:55 +0300

    feat: creating a data fetching function

---
+++ rover/lib/nasa.ts
@@ -0,0 +1,18 @@
+import { APOD } from "@/types/nasa";
+
+const API_KEY = process.env.NASA_API_KEY;
+const BASE_URL = "https://api.nasa.gov";
+
+export async function getAPOD(): Promise<APOD>{
+    const res = await fetch(
+        `${BASE_URL}/planetary/apod?api_key=${API_KEY}`,
+        {
+            next: {revalidate: 86400}, //cache for 24 hours
+        }
+    );
+    if(!res.ok){
+        throw new Error ("Failed to fetch APOD");
+    }
+
+    return res.json();
+}
