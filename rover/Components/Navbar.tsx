7b5bfd7912e9f8066b3a0e65296f3836d9d815c9
Its-Delimas <spencerdelimas@gmail.com>
2026-04-06 17:39:15 +0300

    REDO: NASA publi API down... refactoring code base to countries API with almost similar structure

--- rover/Components/Navbar.tsx
+++ rover/Components/Navbar.tsx
@@ -5,21 +5,20 @@ export default function Navbar() {
         <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-8 py-4 bg-black/80 backdrop-blur-md border-b border-white/10">
 
             {/* Logo */}
-            <Link href="/" className="text-white font-bold text-xl tracking-widest uppercase">
-                Space<span className="text-green-400">Scope</span>
+            <Link href="/" className="text-white font-bold text-xl tracking-widest uppercase ">
+                World<span className="text-green-400">Scope</span>
             </Link>
 
             {/* Nav Links */}
             <div className="flex items-center gap-8">
-                <Link href="/" className="text-white/60 hover:text-white text-sm tracking-wide transition-colors">
-                    Home
-                </Link>
-                <Link href="/apod" className="text-white/60 hover:text-white text-sm tracking-wide transition-colors">
-                    APOD
-                </Link>
-                <Link href="/rovers" className="text-white/60 hover:text-white text-sm tracking-wide transition-colors">
-                    Rovers
-                </Link>
+                <div className="flex items-center gap-8">
+                    <Link href="/" className="text-white/60 hover:text-white text-sm tracking-wide transition-colors">
+                        Home
+                    </Link>
+                    <Link href="/countries" className="text-white/60 hover:text-white text-sm tracking-wide transition-colors">
+                        Countries
+                    </Link>
+                </div>
             </div>
 
         </nav>
--- rover/app/page.tsx
+++ rover/app/page.tsx
@@ -1,7 +1,53 @@
-export default function Home() {
+import { getAPOD } from "@/lib/nasa";
+
+export default async function Home() {
+
+  const apod = await getAPOD();
+
   return (
-    <main>
-      <h1>Rover</h1>
+    <main className='relative min-h-screen'>
+      {/* bg-image */}
+      {apod.media_type === "image" && (
+        <div className="fixed inset-0 bg-cover bg-center opacity-20 -z-10" style={{ backgroundImage: `url(${apod.url})` }} />
+      )}
+
+      {/* Hero content */}
+      <section className="flex flex-col items-center justify-center min-h-screen px-6 text-center">
+        <p className="text-green-400 text-xs tracking-[0.3em] uppercase mb-4">
+          {new Date(apod.date).toLocaleDateString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+
+        <h1 className="text-5x1 md:text-7x1 font-bold text-white max-w-4x1 leading-tight mb-6">
+          {apod.title}
+        </h1>
+
+        <p className="text-white/60 max-w-2x1 text-base leading-relaxed mb-10">
+          {apod.explanation.slice(0, 220)}...
+        </p>
+
+        <div className="flex items-center gap-4">
+          <a href="/apod"
+            className="px-6 py-3 bg-green-400 text-black text-sm font-semibold tracking-wide rounded-full hover:bg-green-300 transition-coolors"
+          > View Full Image</a>
+          <a href="/rovers"
+            className="px-6 py-3 border border-white/20 text-white/70 text-sm tracking-wide rounded-full hover:border-white/50 hover:text-white transition-colors"
+          >
+            Explore Rovers →
+          </a>
+        </div>
+
+        {apod.copyright && (
+          <p className="mt-12 text-white/30 text-xs">
+            Image © {apod.copyright}
+          </p>
+        )}
+
+      </section>
     </main>
   )
 }
