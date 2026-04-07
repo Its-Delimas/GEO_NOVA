8880cbfc588e5ef644ee416f35ba093e487072cf
Its-Delimas <spencerdelimas@gmail.com>
2026-04-08 01:02:59 +0300

    feat: add google site verification meta tag for SEO enhancement

--- geoNova/app/layout.tsx
+++ geoNova/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import "./globals.css";
 import Navbar from '@/Components/Navbar';
 
+<meta name="google-site-verification" content="Hl8z1V53xjMG-j9IN0-MnE9zt96DK8xWBO7jlGLGFR0" />
+
 export const metadata: Metadata = {
   title: "Rover",
   description: "Explore the universe with NASA data",
